2cedc9b2b7ec79df6758a6f02b6ccbdca6ac0870
urgelin <87039322@qq.com>
2013-12-27 19:55:52 +0800

    学习框架
    学习框架

---
+++ admin.php
@@ -0,0 +1,5 @@
+<?php
+define("APP_PATH",'admin/');
+define("DIR_SAFE",true);
+define("DEBUG",true);
+require "hdphp/hdphp.php";
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+define("APP_PATH",'index/');
+define('DEBUG',true);
+define("DIR_SAFE",true);
+require "hdphp/hdphp.php";
